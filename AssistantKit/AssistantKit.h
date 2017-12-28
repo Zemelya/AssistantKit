aae78d453fe6423fa65ad1b934862926dde49966
Vitaly Toroshilov <adba@bk.ru>
2017-12-28 21:42:58 +0200

    Carthage support

---
+++ AssistantKit/AssistantKit.h
@@ -0,0 +1,19 @@
+//
+//  AssistantKit.h
+//  AssistantKit
+//
+//  Created by Vitaly Toroshilov on 28/12/2017.
+//  Copyright © 2017 Mediapark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AssistantKit.
+FOUNDATION_EXPORT double AssistantKitVersionNumber;
+
+//! Project version string for AssistantKit.
+FOUNDATION_EXPORT const unsigned char AssistantKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssistantKit/PublicHeader.h>
+
+
